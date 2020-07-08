9cfee0670447d96726f64529bf2a4714d17a8a55
odillner <o.dillner@gmail.com>
2020-07-08 12:31:18 +0200

    updated userschema

--- models/user.js
+++ models/user.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema({
-    userName: {
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
         type: String,
         required: true
     },
